1a3df9b7bcd4dca6186a2c0fe7ecd7891d2eaab3
Leighton <ljsuderman7@gmail.com>
2021-03-23 18:30:27 -0400

    Set goal complete. Can now view all goals as well

--- app/src/main/java/ca/on/conestogac/alj/studentassistancemanagerandroid/SetGoalActivity.java
+++ app/src/main/java/ca/on/conestogac/alj/studentassistancemanagerandroid/SetGoalActivity.java
@@ -16,16 +16,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 public class SetGoalActivity extends AppCompatActivity {
 
     private Button btnSetGoal;
     private Button btnCancelSet;
-    private TextView txtSetGoalName;
-    private TextView txtSetGoalAmount;
-    private TextInputEditText lblGoalName;
-    private TextInputEditText lblGoalAmount;
+    private TextView txtGoalName;
+    private TextView txtGoalAmount;
+    private TextInputLayout inlGoalName;
+    private TextInputLayout inlGoalAmount;
     private String goalName;
     private double goalAmount;
+    private int cId;
+    private boolean isEditing;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,10 +42,19 @@ public class SetGoalActivity extends AppCompatActivity {
 
         btnSetGoal = findViewById(R.id.btnSetGoal);
         btnCancelSet = findViewById(R.id.btnCancelSet);
-        txtSetGoalName = findViewById(R.id.txtSetGoalName);
-        txtSetGoalAmount = findViewById(R.id.txtSetGoalAmount);
-        lblGoalName = findViewById(R.id.lblGoalName);
-        lblGoalAmount = findViewById(R.id.lblGoalAmount);
+        txtGoalName = findViewById(R.id.txtGoalName);
+        txtGoalAmount = findViewById(R.id.txtGoalAmount);
+        inlGoalName = findViewById(R.id.inlGoalName);
+        inlGoalAmount = findViewById(R.id.inlGoalAmount);
+
+        isEditing = false;
+
+        cId = getIntent().getExtras().getInt("cId");
+        if (cId != 0) {
+            populateFields();
+            isEditing = true;
+            btnSetGoal.setText("Update Goal");
+        }
 
         btnSetGoal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,8 +79,12 @@ public class SetGoalActivity extends AppCompatActivity {
     private void createGoal(){
         Log.i("Goal info:", "\nName: " + goalName + "\nAmount: " + goalAmount);
 
-
-        ((SAMApplication) getApplication()).createCategory(goalName, goalAmount);
+        if (isEditing){
+            ((SAMApplication) getApplication()).updateCategory(cId, goalName, goalAmount);
+        }
+        else{
+            ((SAMApplication) getApplication()).createCategory(goalName, goalAmount);
+        }
 
         Toast.makeText(this, "Budget Goal Created", Toast.LENGTH_SHORT).show();
 
@@ -77,28 +98,63 @@ public class SetGoalActivity extends AppCompatActivity {
     private boolean validateData(){
         boolean validData = true;
 
+
+
         //Name
-        goalName = txtSetGoalName.getText().toString();
+        goalName = txtGoalName.getText().toString();
+
+        List<Category> categories = new ArrayList<>();
+        categories = ((SAMApplication) getApplication()).getAllCategory();
+
+        boolean duplicateGoal = false;
+
+        for (Category c : categories){
+            if(c.getName().equals(goalName) && !isEditing){
+                duplicateGoal = true;
+                validData = false;
+            }
+        }
+
         if(goalName.length() == 0){
-            txtSetGoalName.setError("Budget Goal must have name");
+            inlGoalName.setError("Budget Goal must have name");
             validData = false;
         }
-        if(goalName.length() > 15){
-            txtSetGoalName.setError("Budget Goal name must be 15 or less characters");
+        else if(goalName.length() > 15){
+            inlGoalName.setError("Budget Goal name must be 15 or less characters");
             validData = false;
+        }
+        else if (duplicateGoal){
+            inlGoalName.setError(goalName + " is already a budget goal");
+        }
+        else{
+            inlGoalName.setError(null);
         }
 
         //Amount
-        if (txtSetGoalAmount.getText().length() == 0){
-            txtSetGoalAmount.setError("Must have a budget goal amount");
+        if (String.valueOf(txtGoalAmount.getText()).length() == 0){
+            inlGoalAmount.setError("Must have a budget goal amount");
+            validData = false;
+        }
+        else if (Double.parseDouble(txtGoalAmount.getText().toString()) < 0){
+            inlGoalAmount.setError("Budget Goal Amount cannot be negative");
             validData = false;
         }
         else{
-            goalAmount = Double.parseDouble(txtSetGoalAmount.getText().toString());
+            goalAmount = Double.parseDouble(txtGoalAmount.getText().toString());
+            inlGoalAmount.setError(null);
         }
 
-
         return validData;
+    }
+
+    private void populateFields(){
+        Category category = ((SAMApplication) getApplication()).getCategory(cId);
+        setTitle("Update Budget Goal");
+
+        txtGoalName.setText(category.getName());
+        txtGoalAmount.setText(String.valueOf(category.getGoal()));
+
+        validateData();
     }
 
     @Override
