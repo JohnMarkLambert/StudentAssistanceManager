7852e4268c239635a810e076a44dfcd27264a0b2
John Lambert <JohnMarkLambert@gmail.com>
2021-03-26 10:16:56 -0400

    Dummy Data Records and PaymentType

--- app/src/main/java/ca/on/conestogac/alj/studentassistancemanagerandroid/MainActivity.java
+++ app/src/main/java/ca/on/conestogac/alj/studentassistancemanagerandroid/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,20 +18,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
         //dummy Data
-        ((SAMApplication) getApplication()).addRecord("01/2021", "Food", 1250.50, 1255.99);
-        ((SAMApplication) getApplication()).addRecord("01/2021", "Rent", 1535.20, 1535.20);
-        ((SAMApplication) getApplication()).addRecord("01/2021", "Utilities", 80.85, 70.36);
-        ((SAMApplication) getApplication()).addRecord("01/2021", "Transportation", 125.65, 120.32);
-        ((SAMApplication) getApplication()).addRecord("02/2021", "Food", 1250.50, 1199.99);
-        ((SAMApplication) getApplication()).addRecord("02/2021", "Rent", 1535.20, 1535.20);
-        ((SAMApplication) getApplication()).addRecord("02/2021", "Utilities", 80.85, 90.00);
-        ((SAMApplication) getApplication()).addRecord("02/2021", "Transportation", 125.65, 150.32);
-        ((SAMApplication) getApplication()).addRecord("12/2020", "Food", 1550.50, 1549.99);
-        ((SAMApplication) getApplication()).addRecord("12/2020", "Rent", 1450.00, 1535.20);
-        ((SAMApplication) getApplication()).addRecord("12/2020", "Utilities", 80.85, 80.00);
-        ((SAMApplication) getApplication()).addRecord("12/2020", "Transportation", 125.65, 110.34);
-        ((SAMApplication) getApplication()).addRecord("12/2020", "Xmas Presents", 300.00, 301.45);
+        List<Record> recordList = ((SAMApplication) getApplication()).getAllRecords();
+        if (recordList == null | recordList.isEmpty()) {
+            ((SAMApplication) getApplication()).addRecord("01/2021", "Food", 1250.50, 1255.99);
+            ((SAMApplication) getApplication()).addRecord("01/2021", "Rent", 1535.20, 1535.20);
+            ((SAMApplication) getApplication()).addRecord("01/2021", "Utilities", 80.85, 70.36);
+            ((SAMApplication) getApplication()).addRecord("01/2021", "Transportation", 125.65, 120.32);
+            ((SAMApplication) getApplication()).addRecord("02/2021", "Food", 1250.50, 1199.99);
+            ((SAMApplication) getApplication()).addRecord("02/2021", "Rent", 1535.20, 1535.20);
+            ((SAMApplication) getApplication()).addRecord("02/2021", "Utilities", 80.85, 90.00);
+            ((SAMApplication) getApplication()).addRecord("02/2021", "Transportation", 125.65, 150.32);
+            ((SAMApplication) getApplication()).addRecord("12/2020", "Food", 1550.50, 1549.99);
+            ((SAMApplication) getApplication()).addRecord("12/2020", "Rent", 1450.00, 1535.20);
+            ((SAMApplication) getApplication()).addRecord("12/2020", "Utilities", 80.85, 80.00);
+            ((SAMApplication) getApplication()).addRecord("12/2020", "Transportation", 125.65, 110.34);
+            ((SAMApplication) getApplication()).addRecord("12/2020", "Xmas Presents", 300.00, 301.45);
+        }
+
+        List<List<String>> categoryList = ((SAMApplication) getApplication()).getPaymentTypes();
+        if (categoryList ==  null | categoryList.isEmpty()) {
+            ((SAMApplication) getApplication()).addPaymentType("Debit");
+            ((SAMApplication) getApplication()).addPaymentType("Credit");
+        }
     }
 
     @Override
